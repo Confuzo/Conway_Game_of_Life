f7668141d9251e8a025726d84076888932a3a380
Giovanne <giovannedssantos@gmail.com>
2018-04-19 20:54:50 -0300

    Update header.h

--- include/header.h
+++ include/header.h
@@ -1,41 +1,8 @@
 #define header
 #include <iostream>
 #include <fstream>
+#include "life.h"
+class Life{};
 
-class Life{
-  private:
-    int nLin;
-    int nCol;
-    char ** matriz;
-  public:
-    Life(int Lin, int Col){
+Life reader_file(char*argv){}
 
-        nLin = Lin;
-        nCol = Col;
-    }
-    char type;
-    //we must define how to parse informtaion to here
-    void set_alive(char **  entry){
-      matriz = entry;
-    }
-    void update(){
-
-    }
-    void print(){
-      std::cout << "\nThe currrent conjecture of the life generation is: \n";
-       for(auto i(0); i< nLin; i++){
-         for(auto j(0); j < nCol; j++){
-           std::cout << matriz[i][j];
-         }
-         std::cout << "\n";
-       }
-    }
-    bool stable(){
-      return true;
-    }
-    bool extinct(){
-      return true;
-    }
-};
-
-Life reader_file();
